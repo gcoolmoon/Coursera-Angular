36d495949f51aecad7457fb565cd1168dc3ab97b
gcoolmoon <gcoolmoon@gmail.com>
2016-09-19 01:26:46 +0200

    bonus adds

--- Module1-solution/app.js
+++ Module1-solution/app.js
@@ -5,19 +5,27 @@ angular.module('myFirstAss', [])
 
 .controller('SplitController', function ($scope) {
   $scope.name = "";
+  //$scope.styleColor = function () {
+    // return red;
+  //}
   $scope.splitSent = function () {
     var sent = $scope.name;
     var words = sent.split(",");
     var total = words.filter(Boolean).length;
+    var customColor = 'green';
         //  words.foreach(myFunction)
     if(total==0)
-    $scope.message = "Please enter data first";
+    {customColor = 'red';
+    $scope.message = "Please enter data first";}
     else if (total<=3) {
     $scope.message = "Enjoy";
 
     }
     else {
       $scope.message = "Too much!!!!";
+    }
+    $scope.styleColor = function () {
+      return customColor;
     }
   };
 });
